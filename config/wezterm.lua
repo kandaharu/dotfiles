ff72cd538737f9a97ca925443952e8d82ef20ee5
yu.takahashi <yu.takahashi@andpad.co.jp>
2025-04-17 19:50:15 +0900

    change wezterm settings

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -70,15 +70,9 @@ config.max_fps = 60
 -- ############################################################################
 
 -- ウィンドウの装飾を非表示
-config.window_decorations = "NONE"
+config.window_decorations = "TITLE | RESIZE"
 -- ウィンドウの透明度
 config.window_background_opacity = 0.95
--- フルスクリーンで起動
-local mux = wezterm.mux
-wezterm.on("gui-startup", function(cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
-    window:gui_window():toggle_fullscreen()
-end)
 
 -- ############################################################################
 -- タブ設定
